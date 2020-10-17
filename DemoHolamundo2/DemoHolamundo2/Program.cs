061bf260ecaeabbf370cab5b57d16c178fe81ed5
Michael Asturias <asturias_m@hotmail.com>
2020-10-17 15:13:08 -0600

    Commit1
    Commit1

---
+++ DemoHolamundo2/DemoHolamundo2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DemoHolamundo2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(createMessage());
+        }
+
+        public static String createMessage()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ DemoHolamundo2/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DemoHolamundo2;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Hello World", Program.createMessage());
+        }
+    }
+}
